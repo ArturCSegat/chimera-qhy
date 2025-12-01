23df3343046f6b65d4b8eed18cedcaaf49839d50
Viliam Dias <voliveira@lna.br>
2025-12-01 09:53:46 -0300

    Code formatting applied by Ruff (linter and code formatter tool).

--- chimera_qhy600/instruments/qhy600mdriver.py
+++ chimera_qhy600/instruments/qhy600mdriver.py
@@ -1,15 +1,16 @@
 import ctypes
 import logging
+
 import numpy as np
 
 log: logging.Logger = None
 
-class QHY600MDriver:
 
+class QHY600MDriver:
     def __init__(self, chimera_logger: logging.Logger):
         global log
         log = chimera_logger
-        self.qhyccd = ctypes.CDLL('/usr/local/lib/libqhyccd.so')
+        self.qhyccd = ctypes.CDLL("/usr/local/lib/libqhyccd.so")
         self.qhyccd.GetQHYCCDParam.restype = ctypes.c_double
         self.qhyccd.OpenQHYCCD.restype = ctypes.POINTER(ctypes.c_uint32)
         self.gain = ctypes.c_double(10)
@@ -24,7 +25,7 @@ class QHY600MDriver:
         if result == 0:
             log.info("Init SDK Ok")
         else:
-            raise Exception('SDK not found')
+            raise Exception("SDK not found")
 
         cameras_found = self.qhyccd.ScanQHYCCD()
         if cameras_found > 0:
@@ -48,8 +49,13 @@ class QHY600MDriver:
         pixel_width_um = ctypes.c_uint32(0)
         pixel_height_um = ctypes.c_uint32(0)
         result = self.qhyccd.GetQHYCCDChipInfo(
-            self.camera_handle, ctypes.byref(chip_width_mm), ctypes.byref(chip_height_mm), ctypes.byref(self.image_width),
-            ctypes.byref(self.image_height), ctypes.byref(pixel_width_um), ctypes.byref(pixel_height_um),
+            self.camera_handle,
+            ctypes.byref(chip_width_mm),
+            ctypes.byref(chip_height_mm),
+            ctypes.byref(self.image_width),
+            ctypes.byref(self.image_height),
+            ctypes.byref(pixel_width_um),
+            ctypes.byref(pixel_height_um),
             ctypes.byref(self.bpp),
         )
         log.info(f"GetQHYCCDChipInfo() - result: {result}")
@@ -57,38 +63,59 @@ class QHY600MDriver:
         log.info(f"  Image: {self.image_width.value}x{self.image_height.value} pixels")
         log.info(f"  Pixel: {pixel_width_um.value}x{pixel_height_um.value} um")
         log.info(f"  BPP: {self.bpp.value}")
-    
+
     def close(self):
         # TODO stop exposure is needed?!
-        #self.qhyccd.CancelQHYCCDExposingAndReadout(self.camera_handle)
+        # self.qhyccd.CancelQHYCCDExposingAndReadout(self.camera_handle)
         result = self.qhyccd.CloseQHYCCD(self.camera_handle)
         log.info(f"CloseQHYCCD() - result: {result}")
         result = self.qhyccd.ReleaseQHYCCDResource()
         log.info(f"ReleaseQHYCCDResource() - result: {result}")
-    
+
     def start_exposure(self, exptime):
         log.info(f"start_exposure() - exptime: {exptime} s")
-        self.exposure_time = ctypes.c_double(exptime * 1000000)  # convert seconds to microseconds
+        self.exposure_time = ctypes.c_double(
+            exptime * 1000000
+        )  # convert seconds to microseconds
         self.qhyccd.SetQHYCCDBitsMode(self.camera_handle, self.bpp)
 
         self.qhyccd.SetQHYCCDParam.restype = ctypes.c_uint32
-        self.qhyccd.SetQHYCCDParam.argtypes = [ctypes.c_void_p, ctypes.c_int, ctypes.c_double]
+        self.qhyccd.SetQHYCCDParam.argtypes = [
+            ctypes.c_void_p,
+            ctypes.c_int,
+            ctypes.c_double,
+        ]
 
         self.qhyccd.SetQHYCCDParam(self.camera_handle, ctypes.c_int(6), self.gain)
-        result = self.qhyccd.SetQHYCCDParam(self.camera_handle, ctypes.c_int(8), self.exposure_time)
-        log.info(f"SetQHYCCDParam(CONTROL_EXPOSURE) - result: {result} | exptime: {self.exposure_time.value} us")
+        result = self.qhyccd.SetQHYCCDParam(
+            self.camera_handle, ctypes.c_int(8), self.exposure_time
+        )
+        log.info(
+            f"SetQHYCCDParam(CONTROL_EXPOSURE) - result: {result} | exptime: {self.exposure_time.value} us"
+        )
 
-        self.qhyccd.SetQHYCCDBinMode(self.camera_handle, self.bin_factor, self.bin_factor)
+        self.qhyccd.SetQHYCCDBinMode(
+            self.camera_handle, self.bin_factor, self.bin_factor
+        )
 
-        self.image_width = ctypes.c_uint32(self.image_width.value // self.bin_factor.value)
-        self.image_height = ctypes.c_uint32(self.image_height.value // self.bin_factor.value)
-        self.qhyccd.SetQHYCCDResolution(self.camera_handle, ctypes.c_uint32(0), ctypes.c_uint32(0), self.image_width, self.image_height)
-        
+        self.image_width = ctypes.c_uint32(
+            self.image_width.value // self.bin_factor.value
+        )
+        self.image_height = ctypes.c_uint32(
+            self.image_height.value // self.bin_factor.value
+        )
+        self.qhyccd.SetQHYCCDResolution(
+            self.camera_handle,
+            ctypes.c_uint32(0),
+            ctypes.c_uint32(0),
+            self.image_width,
+            self.image_height,
+        )
+
         self.qhyccd.ExpQHYCCDSingleFrame(self.camera_handle)
         # TODO is chimera already waits simulating exposure time?!
-        #time.sleep(1)
+        # time.sleep(1)
         log.info("start_exposure END")
-
 
     def start_readout(self, mode, top, left, width, height):
         log.info("start_readout INIT")
@@ -98,26 +125,38 @@ class QHY600MDriver:
         height = ctypes.c_uint32()
         bpp = ctypes.c_uint32()
         channel = ctypes.c_uint32()
-        length = self.image_width.value * self.image_height.value * (self.bpp.value // 8)
+        length = (
+            self.image_width.value * self.image_height.value * (self.bpp.value // 8)
+        )
         image_data = (ctypes.c_ubyte * length)()
 
-        self.qhyccd.GetQHYCCDSingleFrame.argtypes = [ctypes.c_void_p, 
-            ctypes.POINTER(ctypes.c_uint32), ctypes.POINTER(ctypes.c_uint32),
-            ctypes.POINTER(ctypes.c_uint32), ctypes.POINTER(ctypes.c_uint32),
-            ctypes.POINTER(ctypes.c_uint8)]
+        self.qhyccd.GetQHYCCDSingleFrame.argtypes = [
+            ctypes.c_void_p,
+            ctypes.POINTER(ctypes.c_uint32),
+            ctypes.POINTER(ctypes.c_uint32),
+            ctypes.POINTER(ctypes.c_uint32),
+            ctypes.POINTER(ctypes.c_uint32),
+            ctypes.POINTER(ctypes.c_uint8),
+        ]
 
         result = self.qhyccd.GetQHYCCDSingleFrame(
-            self.camera_handle, ctypes.byref(width), ctypes.byref(height),
-            ctypes.byref(bpp), ctypes.byref(channel), image_data,
+            self.camera_handle,
+            ctypes.byref(width),
+            ctypes.byref(height),
+            ctypes.byref(bpp),
+            ctypes.byref(channel),
+            image_data,
         )
 
-        log.info(f"GetQHYCCDSingleFrame() - result: {result} | width: {width.value} | height: {height.value} | bpp: {bpp.value} | channel: {channel.value}")
+        log.info(
+            f"GetQHYCCDSingleFrame() - result: {result} | width: {width.value} | height: {height.value} | bpp: {bpp.value} | channel: {channel.value}"
+        )
 
         img_size = width.value * height.value * channel.value * (bpp.value // 8)
         img = np.ctypeslib.as_array(image_data, shape=(img_size,))
         img = img.view(np.uint16).reshape((height.value, width.value))
 
-        log.info("start_readout END")        
+        log.info("start_readout END")
         return img
 
     def get_temperature(self):
